c80558c7527b3ec2c2d860479d29b91cc7e4b554
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-02-11 18:22:12 -0200

    Firebase data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+ courses: any[];
+
+  constructor(private db: AngularFireDatabase) {}
+
+  getCourses() {
+    this.db.list('/courses').valueChanges()
+   .subscribe(result => {
+     this.courses = result;
+     console.log('Firebase Result: ', this.courses);
+   });
+
+  }
+
+
+  ngOnInit(): void {
+    this.getCourses();
+  }
+
+
 }
