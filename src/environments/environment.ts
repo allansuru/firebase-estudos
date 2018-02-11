d1a3349369acd90adef0144717d88e71a1496b48
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-02-11 17:11:21 -0200

    Firebase + angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyA3njUZ9IFF948UQ3Wi75s9v0Xq9c8qXBE',
+    authDomain: 'fir-estudos-437a7.firebaseapp.com',
+    databaseURL: 'https://fir-estudos-437a7.firebaseio.com',
+    projectId: 'fir-estudos-437a7',
+    storageBucket: 'fir-estudos-437a7.appspot.com',
+    messagingSenderId: '681350617503'
+  }
+
 };
