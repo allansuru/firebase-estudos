a4ddce947c8ca28094e9c783169b9191138c5ef7
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-02-12 20:00:59 -0200

    Add item on firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
 
 
 @Component({
@@ -10,24 +11,53 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+ courses$;
+ course$;
+ author$;
 
- courses: any[];
 
   constructor(private db: AngularFireDatabase) {}
 
   getCourses() {
-    this.db.list('/courses').valueChanges()
+    this.courses$ = this.db.list('/courses').valueChanges();
+    this.course$ = this.db.object('/courses/5').valueChanges();
+    this.author$ = this.db.object('/authors/1').valueChanges();
+
+    //#region get sem utilizar o pipe | async
+    /*
+   this.subscription =  this.db.list('/courses').valueChanges()
    .subscribe(result => {
      this.courses = result;
      console.log('Firebase Result: ', this.courses);
    });
+   */
+  //#endregion
 
   }
-
 
   ngOnInit(): void {
     this.getCourses();
   }
 
+  addCourse(course: HTMLInputElement) {
+    const obj: any = {
+      'idade': '15',
+      'nome': course.value,
+      'telefone': 883312,
+      'sections': [
+        {
+          title: 'Seção 1',
+          isFavorite: true
+        },
+        {
+          title: 'Seção 2',
+          isFavorite: false
+        }
+      ]
+    };
 
+    const add = this.db.list('/courses');
+    add.push(obj);
+    course.value = '';
+  }
 }
